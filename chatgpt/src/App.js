4c793f1d16c72d801ffd751fe7b11d3c5b92e6a9
04kash <m04kashish@gmail.com>
2023-06-14 06:52:36 -0400

    added ChatGPT

--- chatgpt/src/App.js
+++ chatgpt/src/App.js
@@ -1,33 +1,11 @@
-import React, { useRef } from 'react';
-import { Editor, tinymce } from '@tinymce/tinymce-react';
+import {React} from 'react';
+import EditorAI from './components/EditorAI';
 
 function App() {
-  return (
-    <>
-    <Editor
-      apiKey="3kwyco0zldkd0ugbqinyz3bgpdqlaiszz61uijjofpqkx6ok"
-      initialValue="<p>This is the initial content of the editor.</p>"
-      init={{
-        height: 500,
-        menubar: false,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount'
-        ],
-        toolbar: 'undo redo | formatselect | ' +
-        'bold italic backcolor | alignleft aligncenter ' +
-        'alignright alignjustify | bullist numlist outdent indent | AskChatGPT' ,
-        content_style: 'div.answer { font-family: Consolas,monaco,monospace;  background-color: #023020; color: white; padding: 3px; }',
-        setup: (editor) => {editor.ui.registry.addButton('AskChatGPT', {text: "Ask ChatGPT",
-    icon: "highlight-bg-color",
-    tooltip: "Highlight a prompt and click this button to query ChatGPT",
-    enabled: true,
-    onAction: (_) => {} },)
-      }}}
-    />
-  </>
-  );
-}
+  return(
+  <EditorAI />)
+  }    
+   
+
 
 export default App;
---
+++ chatgpt/src/components/EditorAI.jsx
@@ -0,0 +1,49 @@
+import {React} from 'react';
+import { Editor} from '@tinymce/tinymce-react';
+import useLLM from "usellm";
+import { useState} from "react";
+
+function EditorAI(){
+    const llm = useLLM({ serviceUrl: "https://usellm.org/api/llm" });
+    const [result, setResult] = useState('');
+  
+    return (
+      <div>
+      <Editor
+        apiKey="3kwyco0zldkd0ugbqinyz3bgpdqlaiszz61uijjofpqkx6ok"
+        initialValue= "<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar: 'undo redo | formatselect | ' +
+          'bold italic backcolor | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | AskChatGPT' ,
+          content_style: 'div.answer { font-family: Consolas,monaco,monospace;  background-color: #023020; color: white; padding: 3px; }',
+          setup: (editor) => {
+      editor.ui.registry.addButton('AskChatGPT', {text: "Ask ChatGPT",
+      icon: "highlight-bg-color",
+      tooltip: "Highlight a prompt and click this button to query ChatGPT",
+      enabled: true,
+      onAction: () => {editor.selection.collapse();
+              editor.execCommand('InsertHTML', false, '<div class="answer"></div>');
+              editor.insertContent(result);
+              llm.chat({
+              messages: [{ role: "user", content: editor.selection.getContent() }],
+              stream: true,
+              onStream: ({ message }) => setResult(message.content)})}
+              
+              
+              
+          });
+  
+          }
+        }} />
+        </div>)
+}
+
+export default EditorAI;
