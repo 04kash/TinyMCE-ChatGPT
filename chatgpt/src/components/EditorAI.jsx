940cf41740afcb188caac2e556df27d78a09a68b
04kash <m04kashish@gmail.com>
2023-06-21 15:22:33 -0400

    updated @gpt prompt interaction

--- chatgpt/src/components/EditorAI.jsx
+++ chatgpt/src/components/EditorAI.jsx
@@ -11,7 +11,7 @@ function EditorAI(){
     return (
       <div>
       <Editor
-         apiKey="API-KEY"
+         apiKey="3kwyco0zldkd0ugbqinyz3bgpdqlaiszz61uijjofpqkx6ok"
         initialValue= "<p>This is the initial content of the editor.</p>"
         init={{
           height: 500,
@@ -29,27 +29,16 @@ function EditorAI(){
           'bold italic backcolor | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | AskChatGPT' ,
           content_style: 'div.answer { font-family: Consolas,monaco,monospace;  background-color: black; color: white; padding: 2px; }',
-      //     setup: function (editor) {
   
-      //  editor.ui.registry.addButton('AskChatGPT', {text: "Ask ChatGPT",
-      //   icon: "highlight-bg-color",
-      // tooltip: "Highlight a prompt and click this button to query ChatGPT",
-      // enabled: true,
-      // onAction: () =>  handleClick(editor)}
-      //  )
-      // }
       setup: function (editor) {
         // Register ChatGPT's reply
         editor.addCommand('reply', async function () {
-      // try {
+      
          const { message } = await llm.chat({
           messages: [{ role: "user", content: editor.getContent()}],
         });
          editor.dom.add(editor.getBody(),'div', {class:"answer"},message.content)
         editor.dom.add(editor.getBody(), 'p', { }, 'Next prompt?')
-      // } catch (error) {
-      //   console.error("Something went wrong!", error);
-      // }
       }
       );
     }
