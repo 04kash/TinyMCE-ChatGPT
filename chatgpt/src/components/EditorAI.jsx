7dfa10aa66107940a9d008ca13b91b1bc4874fc0
04kash <m04kashish@gmail.com>
2023-06-21 15:18:29 -0400

    added @gpt prompt interaction

--- chatgpt/src/components/EditorAI.jsx
+++ chatgpt/src/components/EditorAI.jsx
@@ -4,17 +4,7 @@ import useLLM from "usellm";
 import '../App.css';
 function EditorAI(){
     const llm = useLLM({ serviceUrl: "https://usellm.org/api/llm" });
-    async function handleClick(editor) {
-      try {
-        const { message } = await llm.chat({
-          messages: [{ role: "user", content: editor.selection.getContent()}],
-        });
-        editor.selection.collapse()
-        editor.dom.add(editor.getBody(),'div', {class:"answer"},message.content)
-        editor.dom.add(editor.getBody(), 'p', { }, 'Next prompt?')
-      } catch (error) {
-        console.error("Something went wrong!", error);
-      }}
+    
         
 
 
@@ -31,24 +21,41 @@ function EditorAI(){
             'searchreplace visualblocks code fullscreen',
             'insertdatetime media table paste code help wordcount','textpattern'
           ],
-          //textpattern_patterns: [
-        //{start: '##',cmd:'send'},
+          text_patterns: [
+        {start: '@gpt',cmd:'reply'},
 
-    //],
+    ],
           toolbar: 'undo redo | formatselect | ' +
           'bold italic backcolor | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | AskChatGPT' ,
           content_style: 'div.answer { font-family: Consolas,monaco,monospace;  background-color: black; color: white; padding: 2px; }',
-          setup: function (editor) {
+      //     setup: function (editor) {
   
-       editor.ui.registry.addButton('AskChatGPT', {text: "Ask ChatGPT",
-        icon: "highlight-bg-color",
-      tooltip: "Highlight a prompt and click this button to query ChatGPT",
-      enabled: true,
-      onAction: () =>  handleClick(editor)}
-       )
+      //  editor.ui.registry.addButton('AskChatGPT', {text: "Ask ChatGPT",
+      //   icon: "highlight-bg-color",
+      // tooltip: "Highlight a prompt and click this button to query ChatGPT",
+      // enabled: true,
+      // onAction: () =>  handleClick(editor)}
+      //  )
+      // }
+      setup: function (editor) {
+        // Register ChatGPT's reply
+        editor.addCommand('reply', async function () {
+      // try {
+         const { message } = await llm.chat({
+          messages: [{ role: "user", content: editor.getContent()}],
+        });
+         editor.dom.add(editor.getBody(),'div', {class:"answer"},message.content)
+        editor.dom.add(editor.getBody(), 'p', { }, 'Next prompt?')
+      // } catch (error) {
+      //   console.error("Something went wrong!", error);
+      // }
       }
+      );
+    }
           }}
+
+      
   
           
          />
