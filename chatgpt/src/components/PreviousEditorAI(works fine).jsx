1297c4a70a2836f9979f35677ce747d89a2f2377
04kash <m04kashish@gmail.com>
2023-08-09 11:19:03 -0400

    Update PreviousEditorAI(works fine).jsx

--- chatgpt/src/components/PreviousEditorAI(works fine).jsx
+++ chatgpt/src/components/PreviousEditorAI(works fine).jsx
@@ -46,7 +46,7 @@ function EditorAI(){
           node,
           "span",
           { id: "llmresult" },
-          llmResult.replace(/(?:\r\n|\r|\n)/g, '')
+          llmResult.replace(/(?:\r\n|\r|\n)/g, `&nbsp;`)
         );
         //node.innerHTML = llmResult
         //[llmId[llmId.length-1]]['result']
@@ -335,17 +335,17 @@ function EditorAI(){
     editor.ui.registry.addButton('email', {
       icon:'envelope',
       text: 'Email',
-      onAction: (_) => editor.insertContent(`&nbsp;<p>@ai:This is a template.</p>&nbsp;`)
+      onAction: (_) => editor.insertContent(`<p>@ai:This is a template.</p>`)
     });
     editor.ui.registry.addButton('travel', {
       icon:'travel',
       text: 'Travel Itinerary',
-      onAction: (_) => editor.insertContent(`&nbsp;<p>@ai:This is a template.</p>&nbsp;`)
+      onAction: (_) => editor.insertContent(`<p>@ai:This is a template.</p>`)
     });
     editor.ui.registry.addButton('meeting', {
       icon:'meeting',
       text: 'Meeting Agenda',
-      onAction: (_) => editor.insertContent(`&nbsp;<p>@ai:This is a template.</p>&nbsp;`)
+      onAction: (_) => editor.insertContent(`<p>@ai:This is a template.</p>`)
     });
     editor.ui.registry.addIcon('templates',"<svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z'/></svg>");
     editor.ui.registry.addIcon('envelope','<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>')
